d77c7fa33f53049159e5f44db471fdd76beaf624
Mday1313 <melissaday076@gmail.com>
2019-04-04 18:25:57 -0400

    Firebase connection solid

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,10 @@
-$(document).ready(function () {
+
     
-     // Initialize Firebase
-  var config = {
+// Initialize moment.js
+
+
+//      // Initialize Firebase
+ var config = {
     apiKey: "AIzaSyACJnS8k6G19bohFmZLZ6AaN_N8aKZO4Ow",
     authDomain: "train-scheduler-4b6c6.firebaseapp.com",
     databaseURL: "https://train-scheduler-4b6c6.firebaseio.com",
@@ -10,46 +13,46 @@ $(document).ready(function () {
     messagingSenderId: "259183228175"
   };
   firebase.initializeApp(config);
-
-  // Variable to reference the database 
-  var database = firebase.database();
+//   // Variable to reference the database 
+//   var database = firebase.database();
 
 
 
 // Set initial variables for input fields
 
+  var name = "";
+  var destination = "";
+  var frequency = 0;
+  
 
-
-// Submit button function
-
+// Submit button event listener
+$("#addTrain").on("click", function(event) {
 
     // prevent refresh
+    event.preventDefault();
 
-    
+    // retrieve and store user input in 
+    name = $("#train-name-input").val().trim();
+    console.log(name);
+        // variables 
+            // 
+        
+        // and on firebase
 
+    });
+// create listener to watch for Firebase changes and for loading initial setup 
 
+    // console log snapshot.val() to be sure proper info is collected
 
+    // change html to display proper trains
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // Add in error handler
 
 
 
-
-
-
+// function to access current time 
+    // update next arrival time every minute
+    // update Minutes away every minute
 
 
 
@@ -69,4 +72,18 @@ $(document).ready(function () {
 
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
