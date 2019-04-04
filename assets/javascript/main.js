a28316b4dfe6597d0e1f3f0e545eb26c43db7270
Mday1313 <melissaday076@gmail.com>
2019-04-04 16:51:41 -0400

    beginning psuedocoding

---
+++ assets/javascript/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+    
+     // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyACJnS8k6G19bohFmZLZ6AaN_N8aKZO4Ow",
+    authDomain: "train-scheduler-4b6c6.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4b6c6.firebaseio.com",
+    projectId: "train-scheduler-4b6c6",
+    storageBucket: "train-scheduler-4b6c6.appspot.com",
+    messagingSenderId: "259183228175"
+  };
+  firebase.initializeApp(config);
+
+  // Variable to reference the database 
+  var database = firebase.database();
+
+
+
+// Set initial variables for input fields
+
+
+
+// Submit button function
+
+
+    // prevent refresh
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
